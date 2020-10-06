bb082e019e31b673f711ef8fdd29a0fc702a1fb9
ramdani <ramdani@softwareseni.com>
2020-10-06 23:44:05 +0800

    update routes with routers

--- server.js
+++ server.js
@@ -1,48 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Routes file
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
-
-// Routes
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json(( {
-        success: true,
-        message: "Show all bootcamps"
-    }))
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json(( {
-        success: true,
-        message: `Show bootcamp ${req.params.id}`
-    }))
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json(( {
-        success: true,
-        message: "Create new bootcamp"
-    }))
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json(( {
-        success: true,
-        message: `Update bootcamp ${req.params.id}`
-    }))
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json(( {
-        success: true,
-        message: `Delete bootcamp ${req.params.id}`
-    }))
-});
 
 app.listen(
     PORT,
