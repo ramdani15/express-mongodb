3e6e40f816e118edee4aec7bc7f87ac342fa48e8
ramdani <ramdani@softwareseni.com>
2020-10-06 23:30:04 +0800

    add routes with response

--- server.js
+++ server.js
@@ -8,6 +8,42 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Routes
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json(( {
+        success: true,
+        message: "Show all bootcamps"
+    }))
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json(( {
+        success: true,
+        message: `Show bootcamp ${req.params.id}`
+    }))
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json(( {
+        success: true,
+        message: "Create new bootcamp"
+    }))
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json(( {
+        success: true,
+        message: `Update bootcamp ${req.params.id}`
+    }))
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json(( {
+        success: true,
+        message: `Delete bootcamp ${req.params.id}`
+    }))
+});
+
 app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
