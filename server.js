f44e4c12866b92c207cb6f4ed1a40ab926bb22f1
ramdani <ramdani@softwareseni.com>
2021-01-07 10:19:52 +0800

    rate limiting, hpp & cors

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middlewares/error');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
@@ -50,6 +53,19 @@ app.use(helmet());
 
 // Prevent xss attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
